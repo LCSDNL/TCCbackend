14211e2583b8258efd1168a84bd4976899968a96
LCSDNL <lucasdanielrambo@gmail.com>
2023-05-25 22:03:11 -0300

    ajuste no tratamento de servidor das funcoes e retorno em texto

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var SpeechRecognition = require('./services/SpeechRecognition.js');
 
-app.get('/', function (req, res) {
-  res.send('Olá express!')
-})
+app.get('/audio', async function (req, res) {
+  try {
+    var texto = await SpeechRecognition.fromFile();
+    res.send(texto);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro no processamento');
+  }
+});
 
-app.listen(3000)
+app.listen(3000);
--- services/SpeechRecognition.js
+++ services/SpeechRecognition.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const fs = require("fs");
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
 
@@ -6,30 +6,38 @@ const sdk = require("microsoft-cognitiveservices-speech-sdk");
 const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
 speechConfig.speechRecognitionLanguage = "pt-BR";
 
-function fromFile() {
-    let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("testebr.wav"));
-    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+class SpeechRecognition {
+  static fromFile() {
+    return new Promise((resolve, reject) => {
+      let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("C:/Users/Lucas/Documents/projetoTCC/backend/TCCback/services/testebr.wav"));
+      let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
-    speechRecognizer.recognizeOnceAsync(result => {
+      speechRecognizer.recognizeOnceAsync(result => {
         switch (result.reason) {
-            case sdk.ResultReason.RecognizedSpeech:
-                console.log(`RECOGNIZED: Text=${result.text}`);
-                break;
-            case sdk.ResultReason.NoMatch:
-                console.log("NOMATCH: Speech could not be recognized.");
-                break;
-            case sdk.ResultReason.Canceled:
-                const cancellation = sdk.CancellationDetails.fromResult(result);
-                console.log(`CANCELED: Reason=${cancellation.reason}`);
+          case sdk.ResultReason.RecognizedSpeech:
+            console.log(`RECOGNIZED: Text=${result.text}`);
+            resolve(result.text);
+            break;
+          case sdk.ResultReason.NoMatch:
+            console.log("NOMATCH: Speech could not be recognized.");
+            reject(new Error("Speech could not be recognized."));
+            break;
+          case sdk.ResultReason.Canceled:
+            const cancellation = sdk.CancellationDetails.fromResult(result);
+            console.log(`CANCELED: Reason=${cancellation.reason}`);
 
-                if (cancellation.reason == sdk.CancellationReason.Error) {
-                    console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
-                    console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
-                    console.log("CANCELED: Did you set the speech resource key and region values?");
-                }
-                break;
+            if (cancellation.reason === sdk.CancellationReason.Error) {
+              console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+              console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+              console.log("CANCELED: Did you set the speech resource key and region values?");
+            }
+            reject(new Error("Recognition was canceled."));
+            break;
         }
         speechRecognizer.close();
+      });
     });
+  }
 }
-fromFile();
+
+module.exports = SpeechRecognition;
