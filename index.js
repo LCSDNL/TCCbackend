5edef09ba2bb8b5d8e5d6a95be8c0826a0fb9d90
LCSDNL <lucasdanielrambo@gmail.com>
2023-05-31 22:41:10 -0300

    ajuste na OPENAI

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv/config');
 var express = require('express');
 var app = express();
 var SpeechRecognition = require('./services/SpeechRecognition.js');
+var ResponseAI = require('./services/ResponseCreate.js')
+
 
 app.get('/audio', async function (req, res) {
   try {
@@ -11,5 +14,16 @@ app.get('/audio', async function (req, res) {
     res.status(500).send('Erro no processamento');
   }
 });
+
+
+
+app.get('/resposta', async function(req, res){
+  try{
+    var texto = await ResponseAI.obterRespostaDaAPIOpenAI(req.body[0]);
+    res.send(texto.content)
+  }catch(error){
+    console.error(error);
+    res.status(500).send('Erro no processamento');}
+})
 
 app.listen(3030);
