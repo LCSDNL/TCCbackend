081505ae88113269b09a5552bc5adfcd857cbecd
LCSDNL <lucasdanielrambo@gmail.com>
2023-05-31 22:51:21 -0300

    receber a pergunta por post

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 var SpeechRecognition = require('./services/SpeechRecognition.js');
 var ResponseAI = require('./services/ResponseCreate.js')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 
 app.get('/audio', async function (req, res) {
@@ -17,13 +20,15 @@ app.get('/audio', async function (req, res) {
 
 
 
-app.get('/resposta', async function(req, res){
-  try{
-    var texto = await ResponseAI.obterRespostaDaAPIOpenAI(req.body[0]);
+app.post('/resposta', async function(req, res){
+  const pergunta = req.body.pergunta;
+try{
+    var texto = await ResponseAI.obterRespostaDaAPIOpenAI(pergunta);
     res.send(texto.content)
   }catch(error){
     console.error(error);
     res.status(500).send('Erro no processamento');}
+    
 })
 
 app.listen(3030);
