f437b1911f9745ef44056bd9363a5c8aeed8b591
LCSDNL <lucasdanielrambo@gmail.com>
2023-06-08 23:25:50 -0300

    add CORS police

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ var app = express();
 var SpeechRecognition = require('./services/SpeechRecognition.js');
 var ResponseAI = require('./services/ResponseCreate.js')
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 app.use(bodyParser.json());
+app.use(cors())
 
 
 app.get('/audio', async function (req, res) {
   try {
-    var texto = await fromFile(); // Call the correct function from your 'SpeechRecognition' module
+    var texto = await SpeechRecognition.fromFile(); // Call the correct function from your 'SpeechRecognition' module
     res.send(texto);
   } catch (error) {
     console.error(error);
